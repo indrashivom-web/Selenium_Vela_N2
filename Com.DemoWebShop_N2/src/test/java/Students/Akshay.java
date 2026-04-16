a74a9f2bb731a91f5fc0eae971bdd0ce06b1bf8e
Akshay Seeram <Akshay Seeram@QSpiders>
2026-04-16 20:00:01 +0530

    New Commit By Akshay

--- Com.DemoWebShop_N2/src/test/java/Students/Akshay.java
+++ Com.DemoWebShop_N2/src/test/java/Students/Akshay.java
@@ -1,5 +1,9 @@
 package Students;
 
 public class Akshay {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, I am Akshay. I am a student at XYZ University. I am currently pursuing a degree in Computer Science. I have a passion for programming and software development. In my free time, I enjoy playing video games and exploring new technologies.");
+	}
 
 }
